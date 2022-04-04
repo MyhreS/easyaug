83b20f7dd2ad56550657379d430835269a1b7db1
Simon <simonmyhre1@gmail.com>
2022-04-04 03:17:09 +0200

    Removed clutter

--- Augmentio/image_worker/tests/test_augmenter.py
+++ Augmentio/image_worker/tests/test_augmenter.py
@@ -573,40 +573,5 @@ class TestAugmenter(unittest.TestCase):
             self.assertNotEqual(np.array_equal(image, panda_0), True)
 
 
-
 if __name__ == '__main__':
     unittest.main()
-
-    """
-    def test_augment_image(self):
-        #Tests the augment_image method
-        # Create an Augmenter object.
-        augmenter = Augmenter()
-        # Create a test image.
-        image = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-        # Augment the image.
-        augmented_image = augmenter.augment_image(image)
-        # Check that the image was augmented.
-        self.assertNotEqual(image, augmented_image)
-        # Check that the image was rotated.
-        self.assertNotEqual(image[0, 0], augmented_image[0, 0])
-        # Check that the image was flipped.
-        self.assertNotEqual(image[0, 1], augmented_image[0, 1])
-        # Check that the image was translated.
-        self.assertNotEqual(image[1, 0], augmented_image[1, 0])
-        # Check that the image was scaled.
-        self.assertNotEqual(image[1, 1], augmented_image[1, 1])
-        # Check that the image was sheared.
-        self.assertNotEqual(image[2, 0], augmented_image[2, 0])
-        # Check that the image was rotated and translated.
-        self.assertNotEqual(image[1, 1], augmented_image[1, 1])
-        # Check that the image was rotated and flipped.
-        self.assertNotEqual(image[2, 2], augmented_image[2, 2])
-        # Check that the image was flipped and translated.
-        self.assertNotEqual(image[2, 1], augmented_image[2, 1])
-        # Check that the image was translated and scaled.
-        self.assertNotEqual(image[0, 2], augmented_image[0, 2])
-        # Check that the image was rotated, flipped and translated.
-        self.assertNotEqual(image[2, 2], augmented_image[2, 2])
-        # Check that the image was rotated, flipped, translated and scaled
-    """
