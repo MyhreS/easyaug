6ed57e3ac21aeebeece162cfc673c25fba27c46a
Simon <simonmyhre1@gmail.com>
2022-04-05 23:45:26 +0200

    Update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='easyaug',
     homepage='https://github.com/MyhreS/easyaug.git',
     version='1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=('docs', 'scenarios', 'tests', 'venv')),
     author='Simon Myhre',
     author_email='simonmyhre1@gmail.com',
     description='A package to fast and easy view, prepare and augment images',
