fd20640cea8463f21c939fe925211431833e2bc1
Simon <simonmyhre1@gmail.com>
2022-04-23 12:36:51 +0200

    Update scenario_3.py

--- scenarios/scenario_3.py
+++ scenarios/scenario_3.py
@@ -1,6 +1,6 @@
 """
 Scenario 3:
-Initialize the Preprocesser class from preprocesser to be ready for use.
+Initialize the Preprocesser class from preprocess to be ready for use.
 """
 
 from easyaug.preprocess import Preprocesser
